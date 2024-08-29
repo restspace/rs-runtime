769bb9848aa310cb106eef16d16293810978f3d4
James Ellis-Jones <jamesej@outlook.com>
2024-08-29 10:40:22 +0100

    Switch unzip to use zipjs to allow data descriptors

--- pipeline/unzipSplitter.ts
+++ pipeline/unzipSplitter.ts
@@ -1,35 +1,40 @@
 import { Message } from "rs-core/Message.ts";
 import { AsyncQueue } from "rs-core/utility/asyncQueue.ts";
-import { read } from "../zip/read.ts";
+import { ZipReader } from "https://deno.land/x/zipjs/index.js"
 import { getType } from "rs-core/mimeType.ts";
+import { pathCombine } from "rs-core/utility/utility.ts";
 
 export function unzip(msg: Message): AsyncQueue<Message> {
     const queue = new AsyncQueue<Message>();
-	if (!msg.data) return queue;
+	if (!msg.data) return queue.enqueue(new Error('Unzipping with no data')).close();
 	const readable = msg.data.asReadable();
-	if (!readable) return queue;
-	let anyMessages = false;
+	if (!readable) return queue.enqueue(new Error('Unzipping with no readable')).close();
     (async () => {
 		try {
-			for await (const entry of read(readable)) {
-				const newUrl = msg.url.copy();
-				newUrl.servicePath = newUrl.servicePath + entry.name;
-				if (entry.type === 'file')
-				{
+			const zipReader = new ZipReader(readable);
+			const entries = await zipReader.getEntries();
+			if (entries.length === 0) {
+				queue.enqueue(msg.copy().setNullMessage(true)).close();
+				return;
+			} else {
+				for (const entry of entries) {
+					if (entry.directory) continue;
+					const transformStream = new TransformStream();
+					entry.getData?.(transformStream.writable);
+					const newUrl = msg.url.copy();
+					newUrl.servicePath = pathCombine(newUrl.servicePath, entry.filename);
 					const entryMsg = msg.copy()
-						.setName(entry.name)
-						.setData(entry.body.stream(), getType(entry.name) || '')
+						.setName(entry.filename)
+						.setData(entry.getData ? transformStream.readable : null, getType(entry.filename) || '')
 						.setUrl(newUrl)
 						.removeHeader('transfer-encoding');
 					queue.enqueue(entryMsg);
-					anyMessages = true;
 				}
 			}
 			queue.close();
 		} catch (err) {
 			queue.enqueue(err).close();
 		}
-		if (!anyMessages) queue.enqueue(msg.copy().setNullMessage(true));
 	})();
 
     return queue;
